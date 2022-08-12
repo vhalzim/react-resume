9780a9db448162bd4e30cb8eb61d16d0f01819dd
VhalZim <a_karinao@protonmail.com>
2022-08-12 01:38:56 -0300

    English translate added

--- src/App.js
+++ src/App.js
@@ -5,12 +5,21 @@ import "./index.css"
 
 function App() {
 
+const [spanishActive, setSpanishActive] = useState(true)
 
+
+const setSpanishActiveTrue = ()=>{
+    setSpanishActive(true)
+}
+
+const setSpanishActiveFalse = ()=>{
+    setSpanishActive(false)
+}
 
 
   return (
 <div>
-    <NavBar/>
+    <NavBar setLenguajeToSpanish={setSpanishActiveTrue}  setLenguajeToEnglish={setSpanishActiveFalse} spanishAct={spanishActive} />
     <div className="flex w-screen h-fit md:h-screen py-5  justify-center" id="About">
         <div className="w-[70%] mt-[20%] md:mt-[10%]">
             <div className=" w-[310px] h-[310px] md:float-left md:mx-4  group ml-[-40px] lg:ml-0">
@@ -24,24 +33,36 @@ function App() {
             </div>
 
             <div className="md:my-20">
-                <h1 className=" text-6xl text-[#ff9000]">Sobre Mi</h1>
-                <p>¡Hola Mundo! Mi nombre es Carinao Augusto -aunque la gente se refiere a mi
-                    como Karin-. Soy un estudiante autodidacta de desarrollo web. Actualmente mis
-                    conocimientos se ven limitados al manejo del área de Front-End, pero tengo
-                    planes futuros de continuar mis estudios y extender mis conocimientos para poder
-                    manejar y comprender el mundo del desarrollo web Back-End.</p>
+                <h1 className=" text-6xl text-[#ff9000]">{spanishActive?"Sobre mi":"About me"}</h1>
+                <p>{spanishActive? 
+                "¡Hola Mundo! Mi nombre es Carinao Augusto -aunque la gente se refiere a mi como Karin-. Soy un estudiante autodidacta de desarrollo web. Actualmente mis conocimientos se ven limitados al manejo del área de Front-End, pero tengo planes futuros de continuar mis estudios y extender mis conocimientos para poder manejar y comprender el mundo del desarrollo web Back-End."
+                :
+                "Hello world! My name is Carinao Augusto -tough the people also refers me as Karin-.  I'm a self-taught web development student. Currently, my knowledge is limited to the front-end handling area, but I have future plans to continue my studies and extend my knowledge to handle and understand the Back-end web development world"}
+</p>
             </div>
         </div>
     </div>
     <div className="flex w-screen h-fit md:h-screen  justify-center bg-slate-600 pb-5 pt-20 md:pt-0 md:py-5" id="Skillset">
         <div className="w-[70%] mt-[7%]">
             <h1 className=" text-6xl text-[#ff9000]">Skill set</h1>
+            <p>{spanishActive?
             <p>Mi primer acercamiento a la programación ocurrió en 2021. Aunque desde
                 entonces gran parte del proceso ha consistido en una formación autodidacta, mi
-                principal fuente de consulta ha sido el curso “The Complete 2022 Web Development
-                Bootcamp” dictado por la Doctora Angela Yu en Udemy.com.<br/>
+                principal fuente de consulta ha sido el curso 
+                <em><a href="https://www.udemy.com/course/the-complete-web-development-bootcamp/?src=sac&kw=the+complete+web+development" className=" text-[#ff9000]">
+                The Complete 2022 Web Development
+                Bootcamp</a></em> 
+                dictado por la Doctora Angela Yu en Udemy.com.<br/>
                 En ese curso aprendí conceptos, herramientas y fundamentos de desarrollo web Front-End que
-                después profundicé por mi cuenta. Entre otras, las herramientas mas importantes que hoy en dia manejo son:</p>
+                después profundicé por mi cuenta. Entre otras, las herramientas mas importantes que hoy en dia manejo son:</p>:
+                <p>My first approach to programming occurred in 2022. Tough since then, a big part of the process has consisted in a self-taught formation, 
+                    my primary source of has been the course 
+                    <em><a href="https://www.udemy.com/course/the-complete-web-development-bootcamp/?src=sac&kw=the+complete+web+development" className=" text-[#ff9000]">
+                    The Complete 2022 Web Development Bootcamp</a></em>  dictated by the Dr. Angela Yu in Udemy.com.<br/>
+                    In that course I've learned concept, tools and fundaments of Front-End web 
+                    development that then I delved on my own. Among others, the most important tools I handle today are:</p>
+                }</p>
+            
             <div className="inline-block ml-10 my-8 leading-9">
                 <p><i className="fa-solid fa-caret-right"></i> Html</p>
                 <label>
@@ -58,7 +79,8 @@ function App() {
                 </label>
                 <p><i class="fa-solid fa-caret-right"></i> Git & GitHub</p>
             </div>
-            <p>Actualmente estoy adentrándome en las tecnologías de Node.js y Express.js para introducirme en el área del desarrollo Back-End.</p>
+            
+            <p>{spanishActive? "Actualmente estoy adentrándome en las tecnologías de Node.js y Express.js para introducirme en el área del desarrollo Back-End.":"Currently, I'm approaching to the  Node.js and Expres.js technologies to introduce myself in the Back-end Web developing world"}</p>
         </div>
     </div>
     <div
@@ -66,22 +88,22 @@ function App() {
     id="Portfolio">
     <div className="w-[75%] mt-[7%] text-center">
         <h1 className=" text-6xl text-[#ff9000] ml-[-10px] md:ml-0">Portfolio</h1>
-        <p>Estos son algunos proyectos a los que me he estado dedicando el ultimo tiempo
-            para poner en practica mis conocimientos:</p>
+        <p>{spanishActive? "Estos son algunos proyectos a los que me he estado dedicando el ultimo tiempo para poner en practica mis conocimientos:":"These are some projects that I have been dedicating myself to practice my knowledge:"}
+            </p>
     </div>
     <div
         className="grid grid-cols-1 lg:grid-cols-3 gap-x-3.5 gap-y-3.5 w-fit h-fit px-3 py-2">
 
         <div
             className="flex flex-col justify-between  bg-[rgba(255,144,0,0.2)] w-fit lg:w-80 h-80 portfolio-proyect rounded-md p-6">
-            <a href="https://vhalzim.github.io/dictionary/">
+            <a href="https://vhalzim.github.io/dictionary/" target="_blank">
             <div className="flex justify-between mt-2">
                 <p className="text-4xl">
                     <i class="fa-regular fa-folder"></i>
                 </p>
                 <div className="flex text-xl">
                     <p className="mx-2 hover:text-[#fff]">
-                        <a href="https://github.com/vhalzim/dictionary"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://github.com/vhalzim/dictionary" target="_blank"><i class="fa-brands fa-github"></i></a>
                     </p>
                     <p className="mx-2 hover:text-[#fff]">
                         <i class="fa-solid fa-up-right-from-square"></i>
@@ -90,11 +112,18 @@ function App() {
             </div>
               <div>
                   <h1 className="text-xl ">
-                      <strong>App de Diccionario</strong>
+                      <strong>{spanishActive? "App de Diccionario":"Dictionary-App"}</strong>
                   </h1>
+                  {spanishActive? 
                   <p>Una SPA que utiliza la API de https://dictionaryapi.dev/.<br/>
                       Por desgracia la API solamente funciona en ingles, por lo que la app no puede
                       buscar el significado de palabras en español.</p>
+                      :<p>
+                        An SPA that uses the https://dictionaryapi.dev/ API.<br/> 
+                        Unfortunately, the API only works in English, thereby the app can't 
+                        find the meaning of words in Spanish  
+                    </p>}
+
               </div>
               <div>
                   <p className="text-xs text-slate-500">react.js</p>
@@ -104,45 +133,43 @@ function App() {
 
         <div
             className="flex flex-col justify-between  bg-[rgba(255,144,0,0.2)] w-fit lg:w-80 h-80 portfolio-proyect rounded-md p-6">
-            <a href="https://vhalzim.github.io/clocks2/">
+            <a href="https://vhalzim.github.io/clocks2/" target="_blank">
             <div className="flex justify-between mt-2">
                 <p className="text-4xl">
                     <i class="fa-regular fa-folder"></i>
                 </p>
                 <div className="flex text-xl">
                     <p className="mx-2 hover:text-[#fff]">
-                        <a href="https://github.com/vhalzim/clocks2"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://github.com/vhalzim/clocks2" target="_blank"><i class="fa-brands fa-github"></i></a>
                     </p>
                     <p className="mx-2 hover:text-[#fff]">
-                       <a href="https://vhalzim.github.io/clocks2/"><i class="fa-solid fa-up-right-from-square"></i></a> 
+                       <a href="https://vhalzim.github.io/clocks2/" target="_blank"><i class="fa-solid fa-up-right-from-square"></i></a> 
                     </p>
                 </div>
             </div>
             <div>
                 <h1 className="text-xl ">
-                    <strong>Relojes</strong>
+                    <strong>{spanishActive? "Relojes":"CLocks"}</strong>
                 </h1>
-                <p>Una SPA en formato de carrusel que presenta un conjunto de relojes que
-                    permiten llevar registros de distintas mediciones.
+                <p>{spanishActive? "Una SPA en formato de carrusel que presenta un conjunto de relojes que permiten llevar registros de distintas mediciones.":"A carousel type SPA that shows a set of clocks that allows you to keep different time tracks"}
                 </p>
             </div>
             <div>
                 <p className="text-xs text-slate-500">react.js</p>
             </div>
             </a>
-     
         </div>
 
         <div
             className="flex flex-col justify-between  bg-[rgba(255,144,0,0.2)] w-fit lg:w-80 h-80 portfolio-proyect rounded-md p-6">
-            <a href="https://vhalzim.github.io/Register-Form-React/">
+            <a href="https://vhalzim.github.io/Register-Form-React/" target="_blank">
             <div className="flex justify-between mt-2">
                 <p className="text-4xl">
                     <i class="fa-regular fa-folder"></i>
                 </p>
                 <div className="flex text-xl">
                     <p className="mx-2 hover:text-[#fff]">
-                        <a href="https://github.com/vhalzim/Register-Form-React"><i class="fa-brands fa-github"></i></a>
+                        <a href="https://github.com/vhalzim/Register-Form-React" target="_blank"><i class="fa-brands fa-github"></i></a>
                     </p>
                     <p className="mx-2 hover:text-[#fff]">
                         <i class="fa-solid fa-up-right-from-square"></i>
@@ -151,12 +178,19 @@ function App() {
             </div>
             <div>
                 <h1 className="text-xl ">
-                    <strong>Formulario de registro</strong>
+                    <strong>{spanishActive?"Formulario de registro":"Register-Form"}</strong>
                 </h1>
-                <p>Es un formulario de registro que utiliza el Local Storage para almacenar los
+                {spanishActive? 
+                    <p>Es un formulario de registro que utiliza el Local Storage para almacenar los
                     usuarios.<br/>EL
                     proyecto tuvo la finalidad de practicar el manejo de errores en los inputs de un
                     formulario</p>
+                    :
+                    <p>
+                    This Register-form uses the local storage to keep track of the submitted users.<br/>
+                    The finality of this project was to practice the Error handling in the inputs of a form.
+                    </p>}
+
             </div>
             <div>
                 <p className="text-xs text-slate-500">react.js</p>
@@ -164,7 +198,7 @@ function App() {
             </a>
         </div>
     </div>
-</div>
+    </div>
     <footer className="bg-slate-700 w-screen h-32 text-white pt-3 flex flex-col items-center">
       <p><span className="text-[#ff9000]"><i class="fa-solid fa-at"></i></span> kotsugua@gmail</p>
       <p><span className="text-[#ff9000]"><i class="fa-brands fa-github"></i></span> github.com/vhalzim</p>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 
 
-function NavBar() {
+function NavBar(p) {
   const [buttonState, setButtonState]= useState(true)
   const [alertActive, setAlertActive]= useState(false)
   const [dropdownActive, setDropdownActive] = useState(false)
@@ -20,23 +20,23 @@ function NavBar() {
     <div>
       <nav className="flex justify-between bg-[rgba(0,0,0,0.2)] w-screen h-14 fixed z-10">
         <div>
-          <div className= {buttonState? "bg-[rgba(250,250,250,0.2)] circular-contact-menu": "bg-[rgba(0,0,0,0.2)] open-circular-contact-menu"}>
+          <div className= {buttonState? "bg-[rgba(250,250,250,0.2)] circular-contact-menu": " bg-[rgba(0,0,0,0.8)]  md:bg-[rgba(0,0,0,0.2)] open-circular-contact-menu"}>
             <button className="bg-[#ff9000] px-4 py-3 rounded-full mt-[3%] ml-[3%]" onClick={()=>{setButtonState(!buttonState)}}><i class="fa-solid fa-user"></i></button>
             <label>
-              <p className="absolute text-2xl text-[rgba(0,0,0,0.5)] ml-[60%] mt-[-15%] text-glow" onClick={handleAtButton}><i className="fa-solid fa-at"></i></p>
+              <p className="absolute text-2xl text-[#ff9000] md:text-[rgba(0,0,0,0.5)] ml-[60%] mt-[-15%] text-glow " onClick={handleAtButton}><i className="fa-solid fa-at"></i></p>
               <span className="absolute bg-[rgba(255,255,255,0.5)] px-1 text-xs rounded-md ml-[15%] mt-[2%] visible" ><em>kotsugua@gmail.com</em></span>
-              <span className={alertActive? "copied-alert-true":"copied-alert-false"}><em>copiado al portapapeles</em></span>
+              <span className={alertActive? "copied-alert-true":"copied-alert-false"}><em>{p.spanishAct? "copiado al portapapeles":"copied to clipboard"}</em></span>
             </label>
 
 
             <label>
-              <p className="absolute text-2xl text-[rgba(0,0,0,0.5)] ml-[50%] mt-[5%] text-glow"><a href="https://github.com/vhalzim"><i className="fa-brands fa-github"></i></a></p>
+              <p className="absolute text-2xl text-[#ff9000] md:text-[rgba(0,0,0,0.5)] ml-[50%] mt-[5%] text-glow "><a href="https://github.com/vhalzim" target="_blank"><i className="fa-brands fa-github"></i></a></p>
              <span className="absolute bg-[rgba(255,255,255,0.5)] px-1 text-xs rounded-md ml-[-10%] mt-[23%] visible"><em>https://github.com/vhalzim</em></span>
             </label>
 
-            <p className="absolute text-2xl text-[rgba(0,0,0,0.5)] ml-[30%] mt-[22%] text-glow"><a href="https://wa.me/5493584335701"><i class="fa-brands fa-whatsapp"></i></a></p>
+            <p className="absolute text-2xl text-[#ff9000] md:text-[rgba(0,0,0,0.5)] ml-[30%] mt-[22%] text-glow "><a href="https://wa.me/5493584335701" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></p>
 
-            <p className="absolute text-2xl text-[rgba(0,0,0,0.5)] ml-[5%] mt-[35%] text-glow"><a href="https://t.me/VhalZim"><i class="fa-brands fa-telegram"></i></a></p>
+            <p className="absolute text-2xl text-[#ff9000] md:text-[rgba(0,0,0,0.5)] ml-[5%] mt-[35%] text-glow "><a href="https://t.me/VhalZim" target="_blank"><i class="fa-brands fa-telegram"></i></a></p>
           </div>
           <p className={buttonState? "contact-p":"contact-p-hidden"}>&lt;-contacto</p>
         </div>
@@ -48,8 +48,8 @@ function NavBar() {
         </div>
       </nav>
       <div className={dropdownActive? "fixed right-[2%] top-14 dropdown-on": "fixed right-[2%] top-14 dropdown-off"}>
-          <button className="border-x bordder-b border-[#ff9000] px-3 hover:bg-[rgba(255,144,0,0.2)]" onClick ={()=>{alert("español")}}>Español</button><br/>
-          <button className="border border-[#ff9000] rounded-b-md px-3 hover:bg-[rgba(255,144,0,0.2)]" onClick ={()=>{alert("english")}}>English</button>
+          <button className="border-x bordder-b border-[#ff9000] px-3 hover:bg-[rgba(255,144,0,0.2)]" onClick ={()=>{p.setLenguajeToSpanish()}}>Español</button><br/>
+          <button className="border border-[#ff9000] rounded-b-md px-3 hover:bg-[rgba(255,144,0,0.2)]" onClick ={()=>{p.setLenguajeToEnglish()}}>English</button>
         </div>
     </div>
 
